51092948ae5a5e6e42e7fcad8905e26695727253
Levy Porfirio <levyporfirio@edu.unifor.br>
2025-11-05 15:29:47 -0300

    socorrista

--- socorrista/iten_1.gd
+++ socorrista/iten_1.gd
@@ -3,4 +3,4 @@ extends TextureButton
 @export var item_name: String = ""
 
 func _pressed():
-	get_tree().call_group("level_controller", "on_item_selected", item_name)
+	get_tree().call_group( "on_item_selected", item_name)
--- socorrista/level1.gd
+++ socorrista/level1.gd
@@ -1,54 +1,73 @@
 extends Node2D
 
-@export var item_correto: String = "Álcool"
+@export var item_correto: String = ""
 @export var estado: int = 0
 
 var selected_item: String = ""
 var estado_anterior: int = -1
+var erro_timer := 0.0
+var erro_visivel := false
 
-func _ready():
-	add_to_group("level_controller")
 
 func _process(delta):
 	if estado != estado_anterior:
 		estado_anterior = estado
-		
-		if estado == 1:
-			$BraçoMachucado/Sprite2D.texture = load("res://sprites socorristas/braço quebrado x ray.png")
-			$Intro1.text = "algo"
-			
-		elif estado == 2:
-			$BraçoMachucado/Sprite2D.texture = load("res://sprites socorristas/braço quebrado x ray.png")
-			$Intro1.text = "algo2"
-			
-		elif estado == 3:
-			$BraçoMachucado/Sprite2D.texture = load("res://sprites socorristas/braço estendido bandagens.png")
-			$Intro1.text = "algo3"
-			
 
-func ver_se_certo(item :String):
-	var certo = ""
-	if estado == 0:
-		certo = "a"
-	elif estado == 1:
-		certo = "p"
-	elif estado == 2:
-		certo = "b"
-		
-	if item == certo:
-		estado+=1
-	
-	
+		match estado:
+			
+			1:
+				$BraçoMachucado/Sprite2D.texture = load("res://sprites socorristas/braço quebrado x ray.png")
+				$Intro.text = "Otimo, agora vamos passar a\npomada no ferimento..."
+			2:
+				$BraçoMachucado/Sprite2D.texture = load("res://sprites socorristas/braço quebrado x ray.png")
+				$Intro.text = "Excelente! Agora vamos enfaixar o\nobraço para concluir..."
+			3:
+				$BraçoMachucado/Sprite2D.texture = load("res://sprites socorristas/braço estendido bandagens.png")
+				$Intro.text = "Ótimo trabalho, paciente liberado!"
+
+	# Se a mensagem de erro estiver visível, conta o tempo e some depois de 2 segundos
+	if erro_visivel:
+		erro_timer -= delta
+		if erro_timer <= 0:
+			erro_visivel = false
+			$MsgErro.visible = false
+
+
+func ver_se_certo(item: String):
+	var item_certo_por_estado = {
+		0: "Alcool",
+		1: "Pomada",
+		2: "Bandagem"
+	}
+
+	if item_certo_por_estado.has(estado):
+		if item == item_certo_por_estado[estado]:
+			estado += 1
+			$MsgErro.visible = false
+			erro_visivel = false
+		else:
+			mostrar_erro("  Item incorreto!\nTente novamente.")
+
+
+func mostrar_erro(msg: String):
+	var label = $MsgErro
+	label.text = msg
+	label.visible = true
+	erro_timer = 2.0
+	erro_visivel = true
+
+
 func _on_item_1_pressed() -> void:
 	selected_item = "Alcool"
-	ver_se_certo("a")
-
+	ver_se_certo(selected_item)
 
 func _on_item_2_pressed() -> void:
 	selected_item = "Bandagem"
-	ver_se_certo("b")
-
+	ver_se_certo(selected_item)
 
 func _on_item_3_pressed() -> void:
 	selected_item = "Pomada"
-	ver_se_certo("p")
+	ver_se_certo(selected_item)
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_select.tscn")
